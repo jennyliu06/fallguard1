6927be1e0b1a848a120e4a1ce1b96ff4e281a60c
jennyliu06 <jiayiliu1116@gmail.com>
2024-04-10 22:53:54 -0400

    added data streaming ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,15 @@ import pandas as pd
 import numpy as np
 import csv
 
+import dash
+from dash.dependencies import Output, Input
+from dash import dcc, html, dcc
+from datetime import datetime
+import json
+import plotly.graph_objs as go
+from collections import deque
+from flask import Flask, request
+
 def main():
     # Page title
     st.set_page_config(page_title='Fall Guard', page_icon='🤖')
@@ -71,7 +80,7 @@ def main():
                 #st.write("Row:", row)  # Debug statement to check each row
                 if row["First Name"] == username and row["Password"] == password:
                     st.success("Logged in as {}".format(username))
-                    task = st.sidebar.selectbox("User Menu", ["Profile", "Data"])
+                    task = st.sidebar.selectbox("User Menu", ["Profile", "Data", "Falls"])
                     password_matched = True
                     if choice == "Data":
                         st.subheader("Falls")
@@ -127,6 +136,77 @@ def main():
                                     st.success("Fall detected! Updated Google Sheet.")
                                     break
                     break
+                    if choice == "Falls":
+                        server = Flask(__name__)
+                        app = dash.Dash(__name__, server=server)
+
+                        MAX_DATA_POINTS = 1000
+                        UPDATE_FREQ_MS = 100
+
+                        time = deque(maxlen=MAX_DATA_POINTS)
+                        accel_x = deque(maxlen=MAX_DATA_POINTS)
+                        accel_y = deque(maxlen=MAX_DATA_POINTS)
+                        accel_z = deque(maxlen=MAX_DATA_POINTS)
+
+                        app.layout = html.Div(
+                            [
+                                dcc.Markdown(
+                                    children="""
+                                    # Live Sensor Readings
+                                    Streamed from Sensor Logger: tszheichoi.com/sensorlogger
+                                """
+                                ),
+                                dcc.Graph(id="live_graph"),
+                                dcc.Interval(id="counter", interval=UPDATE_FREQ_MS),
+                            ]
+                        )
+
+
+                        @app.callback(Output("live_graph", "figure"), Input("counter", "n_intervals"))
+                        def update_graph(_counter):
+                            data = [
+                                go.Scatter(x=list(time), y=list(d), name=name)
+                                for d, name in zip([accel_x, accel_y, accel_z], ["X", "Y", "Z"])
+                            ]
+
+                            graph = {
+                                "data": data,
+                                "layout": go.Layout(
+                                    {
+                                        "xaxis": {"type": "date"},
+                                        "yaxis": {"title": "Acceleration ms<sup>-2</sup>"},
+                                    }
+                                ),
+                            }
+                            if (
+                                len(time) > 0
+                            ):  #  cannot adjust plot ranges until there is at least one data point
+                                graph["layout"]["xaxis"]["range"] = [min(time), max(time)]
+                                graph["layout"]["yaxis"]["range"] = [
+                                    min(accel_x + accel_y + accel_z),
+                                    max(accel_x + accel_y + accel_z),
+                                ]
+
+                            return graph
+
+
+                        @server.route("/data", methods=["POST"])
+                        def data():  # listens to the data streamed from the sensor logger
+                            if str(request.method) == "POST":
+                                print(f'received data: {request.data}')
+                                data = json.loads(request.data)
+                                for d in data['payload']:
+                                    if (
+                                        d.get("name", None) == "accelerometer"
+                                    ):  #  modify to access different sensors
+                                        ts = datetime.fromtimestamp(d["time"] / 1000000000)
+                                        if len(time) == 0 or ts > time[-1]:
+                                            time.append(ts)
+                                            # modify the following based on which sensor is accessed, log the raw json for guidance
+                                            accel_x.append(d["values"]["x"])
+                                            accel_y.append(d["values"]["y"])
+                                            accel_z.append(d["values"]["z"])
+                            return "success"
 
             if not password_matched:
                 st.warning("Incorrect Name/Password.")
